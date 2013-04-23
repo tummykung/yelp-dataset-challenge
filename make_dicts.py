aa796dd19a443673f7b7d5ad9d7cbcc4c4bc385b
Tum Chaturapruek <tummykung@gmail.com>
2013-04-23 02:08:32 -0700

    update the preprocessing code, prepare for MF

--- make_dicts.py
+++ make_dicts.py
@@ -1,24 +1,81 @@
 import sys
+import pickle
 
 def make_dicts(filename):
-	data = []
+  data = []
+  with open(filename) as f:
+    for line in f.readlines():
+      line = line.replace('true','True').replace('false','False').strip()
+      try:
+        data.append(eval(line))
+      except:
+        print 'Error occured with line\n%s'%line
+  return data
 
-	with open(filename) as f:
-		for line in f.readlines():
-			line = line.replace('true','True').replace('false','False').strip()
-			try:
-				data.append(eval(line))
-			except:
-				print 'Error occured with line\n%s'%line
+def make_hash_map(list_of_things, output_filename = ""):
+  output = {}
 
-	return data
+  for i in range(len(list_of_things)):
+    output[list_of_things[i]] = i
 
+  if (output_filename != ""):
+    with open(output_filename, 'w+') as filename:
+      pickle.dump(output, filename)
+  return output
+
+def make_reverse_hash_map(hash_map, output_filename = ""):
+  output ={hash_map[key]: key for key in hash_map}
+    
+  if output_filename != "":
+    with open(output_filename, 'w+') as filename:
+      pickle.dump(output, filename)
+      
+  return output
+
+def make_user_hashes():
+  users = make_dicts('yelp_phoenix_academic_dataset/yelp_academic_dataset_user.json')
+  list_of_user_ids = []
+  for user in users:
+    list_of_user_ids += [user["user_id"]]
+  user_hash = make_hash_map(list_of_user_ids, 'user_id2num.txt')
+  make_reverse_hash_map(user_hash, 'user_num2id.txt')
+
+def make_business_hashes():
+  businesses = make_dicts('yelp_phoenix_academic_dataset/yelp_academic_dataset_business.json')
+  list_of_business_ids = []
+  for business in businesses:
+    list_of_business_ids += [business["business_id"]]
+  business_hash = make_hash_map(list_of_business_ids, 'business_id2num.txt')
+  make_reverse_hash_map(business_hash, 'business_num2id.txt')
+
+
+def make_graph():
+  businesses = make_dicts('yelp_phoenix_academic_dataset/yelp_academic_dataset_business.json')
+  reviews = make_dicts('yelp_phoenix_academic_dataset/yelp_academic_dataset_review.json')
+
+  with open('user_id2num.txt', 'r') as filename:
+      user_id2num = pickle.load(filename)
+  with open('business_id2num.txt', 'r') as filename:
+      business_id2num = pickle.load(filename)
+      
+  output = []
+  for i in range(len(reviews)):
+    output += [(user_id2num[reviews[i]["user_id"]], business_id2num[reviews[i]["business_id"]], reviews[i]["stars"])]
+
+  
+  f = open('temporary.txt', 'w+')
+  for edge in output:
+    newline = '%d %d %g\n' %edge
+    f.write(newline)
+  f.close()
+  return output
 
 if __name__ == '__main__':
-	try:
-		filename = sys.argv[1]
-	except:
-		filename = 'yelp_academic_dataset_business.json'
+  try:
+    filename = sys.argv[1]
+  except:
+    filename = 'yelp_phoenix_academic_dataset/yelp_academic_dataset_business.json'
 
-	print len(make_dicts(filename))
-
+  #dicts = make_dicts(filename)
+  make_graph()
+  
---
+++ test.sh
@@ -0,0 +1,25 @@
+# For details please visit Danny Bickson's blog:
+# http://bickson.blogspot.hu/2012/12/collaborative-filtering-with-graphchi.html.
+# The source for ALS, SGD and other MF algorithms: /home/daroczyb/graphchi/src/graphchi/toolkits/collaborative_filtering
+
+TRAINING='test_graph' # training set
+K=2        # dimension of latent feature space
+MAXITER=10 # maximum number of iterations
+THREADS=16 # number of threads
+
+
+echo "Alternate least square (ALS) on "$TRAINING
+./als --matrixmarket=true --training=$TRAINING --D=$K --maxiter=$MAXITER --ncpus=$THREADS --quiet=1
+echo "Stochastic Gradient Descent (SGD) on "$TRAINING
+./sgd --matrixmarket=true --training=$TRAINING --D=$K --maxiter=$MAXITER --ncpus=$THREADS --quiet=1
+
+TRAINING='test_graph_mm' # training set
+VALIDATION='test_graph_mme' # validation set
+K=5
+
+echo "Alternate least square (ALS) on "$TRAINING
+./als --matrixmarket=true --training=$TRAINING --validation=$VALIDATION --D=$K --maxiter=$MAXITER --ncpus=$THREADS --minval=1 --maxval=5 --quiet=1
+echo "Stochastic Gradient Descent (SGD) on "$TRAINING
+./sgd --matrixmarket=true --training=$TRAINING --validation=$VALIDATION --D=$K --maxiter=$MAXITER --ncpus=$THREADS --minval=1 --maxval=5 --quiet=1
+
+
